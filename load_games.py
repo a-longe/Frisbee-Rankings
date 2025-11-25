6dfb6c61e8798bea10c5d42efef7a36c6d3ecbb9
Alex Longe <alex.m.longe@gmail.com>
2025-11-25 15:09:32 -0800

    working load_2025_results

--- load_games.py
+++ load_games.py
@@ -1,22 +1,22 @@
-from typing import NamedTuple
-from numpy.char import isdigit
+from functools import total_ordering
 import requests
 
-from time import sleep
+from time import sleep, time
 
 import pandas as pd
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 session = requests.Session()
-url = "https://play.usaultimate.org/events/tournament/"
+URL = "https://play.usaultimate.org/events/tournament/"
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--blink-settings=imagesEnabled=false")
 driver = webdriver.Chrome(options=options)
 
@@ -24,10 +24,25 @@ RESULT_DATE = 0
 RESULT_SCORE = 1
 RESULT_OPP = 2
 
-SCRAPE_PERIOD = 60*60
-# 5 pages, 85 tournaments, 100 events x 20 teams
-TOTAL_ACTIONS = 5+85+(100*20)
-DELAY_BETWEEN_ACTIONS = SCRAPE_PERIOD / TOTAL_ACTIONS
+# 60min * 60sec = 1h
+SEC_PER_HR = 60*60
+SCRAPE_PERIOD = 1*SEC_PER_HR
+# found in testing for 2025 season
+TOTAL_ACTIONS = 1238
+# 1-0.78 = 0.22, expected time spend fufilling requests
+DELAY_BETWEEN_ACTIONS = (SCRAPE_PERIOD / TOTAL_ACTIONS)*0.78
+
+
+
+# total request count
+# - any javascript redirects...
+total_request_count = 0
+
+def get_url_w_delay(cur_driver: WebDriver, url):
+    global total_request_count
+    total_request_count = total_request_count + 1
+    cur_driver.get(url)
+    sleep(DELAY_BETWEEN_ACTIONS)
 
 
 def get_tournament_urls_on_page(cur_page: WebDriver) -> list[str]:
@@ -39,7 +54,7 @@ def get_tournament_urls_on_page(cur_page: WebDriver) -> list[str]:
 
 
 def get_2025_tournaments() -> list[str]:
-    driver.get(url)
+    get_url_w_delay(driver, URL)
 
     submit_btn = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "CT_HP_Mid_1_btnSubmit"))
@@ -86,15 +101,16 @@ def get_2025_tournaments() -> list[str]:
 
 
 def get_event_urls(tournament_url) -> list[str]:
-    driver.get(tournament_url)
+    get_url_w_delay(driver, tournament_url)
 
-    events_element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "div.eventInfo2"))
-    )
-
-    mens_section_element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "dl.groupType"))
-    )
+    try:
+        mens_section_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "dl.groupType"))
+        )
+    except TimeoutException:
+        # tournament information has not been posted
+        print(f"WARNING: Events have not been posted: {tournament_url}")
+        return []
 
     button_elements = mens_section_element.find_elements(By.TAG_NAME, "input")
     event_buttons = [button for button in button_elements if "College" in button.get_attribute("value")]
@@ -115,7 +131,9 @@ def get_event_urls(tournament_url) -> list[str]:
         button_elements = mens_section_element.find_elements(By.TAG_NAME, "input")
         event_button = [button for button in button_elements \
                         if "College" in button.get_attribute("value")][btn_i]
+
         event_button.click()
+        sleep(1)
 
         _ = WebDriverWait(driver, 10).until(
             lambda d: driver.current_url != tournament_url
@@ -129,11 +147,16 @@ def get_event_urls(tournament_url) -> list[str]:
 
 
 def get_team_urls(event_url) -> list[str]:
-    driver.get(event_url)
+    get_url_w_delay(driver, event_url)
 
-    pools = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.pool"))
-    )
+    try:
+        pools = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.pool"))
+        )
+    except TimeoutException:
+        # pools have not been posted yet
+        print(f"WARNING: Pools not posted: {event_url}")
+        return []
 
     team_links = []
     for pool in pools:
@@ -144,10 +167,27 @@ def get_team_urls(event_url) -> list[str]:
 
 
 def get_results(tournament_url) -> pd.DataFrame:
-    pass
+    event_urls = get_event_urls(tournament_url)
+    team_urls = [team_url for event_url in event_urls \
+        for team_url in get_team_urls(event_url)]
+
+
+    tournament_results = [get_team_results(team_url) for team_url in team_urls]
+
+    if len(tournament_results) == 0:
+        return pd.DataFrame() # empty
+
+    return pd.concat(tournament_results).reset_index()\
+        .drop(columns=['index']).drop_duplicates()
+
 
 def get_team_results(team_event_url) -> pd.DataFrame:
-    driver.get(team_event_url)
+    get_url_w_delay(driver, team_event_url)
+
+    if driver.current_url == "https://play.usaultimate.org/":
+        # team has been deleted
+        print(f"WARNING: Team has been deleted {team_event_url}")
+        return pd.DataFrame()
 
     results_table = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "CT_Right_0_gvEventScheduleScores"))
@@ -160,7 +200,7 @@ def get_team_results(team_event_url) -> pd.DataFrame:
 
     # remove acroym and space between
     team_name = unformatted_team_name.split('(')[0].strip()
-    if "-B" in team_name:
+    if team_name.endswith("-B"):
         team_name = team_name.replace("-B", "") # remove "-B"
         team_name += " [B]"
 
@@ -176,6 +216,10 @@ def get_team_results(team_event_url) -> pd.DataFrame:
         score_str = result_element\
             .find_elements(By.TAG_NAME, "span")[1]\
             .text
+
+        if len(score_str) == 0:
+            # score not reported
+            continue
         score_tuple = (score_str.split()[0], score_str.split()[-1])
         opponent = result_element\
             .find_elements(By.TAG_NAME, "span")[2]\
@@ -191,5 +235,17 @@ def get_team_results(team_event_url) -> pd.DataFrame:
         columns=["Date String", "Tournament", "Score(F,A)", "Team Name", "Opponent"])
 
 def get_2025_results() -> pd.DataFrame:
-    pass
+    tourny_urls = get_2025_tournaments()
+    results = []
+    count = 0
+    start_time = time()
+    for tourny_url in tourny_urls:
+        print(f"Completed {count}/82: {100*total_request_count/TOTAL_ACTIONS:.2f}%", end='\r')
+        results.append(get_results(tourny_url))
+        count += 1
 
+    return pd.concat(results).reset_index()\
+        .drop(columns=['index']).drop_duplicates()
+    print(f"Total Time = {time() - start_time:.1f}s")
+    print(f"Total Requests = {total_request_count}")
+    return results
